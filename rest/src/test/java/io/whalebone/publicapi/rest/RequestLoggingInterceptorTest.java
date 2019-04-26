9ec1e36084cb9f01e26771940e3c00b56448c892
Tomas Kozel <kozelto@gmail.com>
2019-04-26 14:39:30 +0200

    API requests logged as json

--- rest/src/test/java/io/whalebone/publicapi/rest/RequestLoggingInterceptorTest.java
+++ rest/src/test/java/io/whalebone/publicapi/rest/RequestLoggingInterceptorTest.java
@@ -1,6 +1,12 @@
 package io.whalebone.publicapi.rest;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import org.apache.commons.codec.Charsets;
 import org.apache.commons.lang3.StringUtils;
+import org.hamcrest.CoreMatchers;
 import org.jboss.resteasy.specimpl.MultivaluedTreeMap;
 import org.mockito.*;
 import org.testng.annotations.BeforeMethod;
@@ -8,7 +14,6 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.core.MultivaluedHashMap;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.UriInfo;
 import java.io.ByteArrayInputStream;
@@ -17,17 +22,17 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.startsWith;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import com.google.common.io.Resources;
 
 public class RequestLoggingInterceptorTest {
-
     @Mock
     private Logger logger;
-
     @Mock
     private ContainerRequestContext ctx;
 
@@ -45,14 +50,17 @@ public class RequestLoggingInterceptorTest {
     @DataProvider
     public Object[][] logRecordTest_data() {
         return new Object[][] {
-                {"GET", "http://host/end/point?query=string&param=value", createHeaders(), "", getExpectedRecord1()},
-                {"POST", "http://host/end/point?query=string&param=value", createHeaders(), "SomeBody", getExpectedRecord2()}
+                {"GET", "http://host/end/point?query=string&param=value", createHeaders(), "", "get-request-logged"},
+                {"POST", "http://host/end/point?query=string&param=value", createHeaders(), "SomeBody", "post-request-logged"}
         };
     }
 
     @Test(dataProvider = "logRecordTest_data")
-    public void logRecordTest(String method, String uri, MultivaluedMap<String, String> headers, String body, String expectedLogRecord)
+    public void logRecordTest(String method, String uri, MultivaluedMap<String, String> headers, String body, String expectedJsonFile)
             throws Exception {
+        String expectedJson = Resources.toString(Resources.getResource("io/whalebone/publicapi/rest/json/" + expectedJsonFile + ".json"), Charsets.toCharset("UTF-8"));
+        JsonParser parser = new JsonParser();
+        JsonElement expected = parser.parse(expectedJson);
         when(ctx.getMethod()).thenReturn(method);
         UriInfo uriInfo = mock(UriInfo.class);
         URI uriObject = URI.create(uri);
@@ -67,7 +75,9 @@ public class RequestLoggingInterceptorTest {
         interceptor.filter(ctx);
 
         verify(logger).log(eq(Level.INFO), logCaptor.capture());
-        assertThat(logCaptor.getValue(), is(expectedLogRecord));
+        assertThat(logCaptor.getValue(), startsWith(RequestLoggingInterceptor.LOG_PREFIX));
+        JsonElement logged = parser.parse(logCaptor.getValue().replace(RequestLoggingInterceptor.LOG_PREFIX, ""));
+        assertThat(logged, is(expected));
     }
 
     private static MultivaluedMap<String, String> createHeaders() {
@@ -78,35 +88,5 @@ public class RequestLoggingInterceptorTest {
         headers.add("EmptyHeader", "");
         headers.add("NullHeader", null);
         return headers;
-    }
-
-    private static String getExpectedRecord1() {
-        return "WB API Request:\n" +
-                "\tmethod: GET\n" +
-                "\turi: http://host/end/point?query=string&param=value\n" +
-                "\theaders:\n" +
-                "\t\tAuthorization: Bearer <token>\n" +
-                "\t\tEmptyHeader: \n" +
-                "\t\tMultivalueHeader: \n" +
-                "\t\t\tmultival1\n" +
-                "\t\t\tmultival2\n" +
-                "\t\tNullHeader: null\n" +
-                "\t\tSingleValueHeader: headerVal1\n" +
-                "\tbody: \n";
-    }
-
-    private static String getExpectedRecord2() {
-        return "WB API Request:\n" +
-                "\tmethod: POST\n" +
-                "\turi: http://host/end/point?query=string&param=value\n" +
-                "\theaders:\n" +
-                "\t\tAuthorization: Bearer <token>\n" +
-                "\t\tEmptyHeader: \n" +
-                "\t\tMultivalueHeader: \n" +
-                "\t\t\tmultival1\n" +
-                "\t\t\tmultival2\n" +
-                "\t\tNullHeader: null\n" +
-                "\t\tSingleValueHeader: headerVal1\n" +
-                "\tbody: SomeBody\n";
     }
 }
